0a4cac7434a6bd823e9ada33b003aa4412b6e50d
LookItVal <86853474+LookItVal@users.noreply.github.com>
2023-12-10 13:50:01 -0600

    removed things causing problems

--- server/chunks/app/_nuxt/index-styles.e1dfe05b.mjs
+++
@@ -1,6 +0,0 @@
-const index_vue_vue_type_style_index_0_lang = ".home-window{height:100vh;width:100vw}.home-window,.temp-portfolio{align-items:center;display:flex;flex-direction:column;flex-wrap:wrap;justify-content:space-around}";
-
-const indexStyles_e1dfe05b = [index_vue_vue_type_style_index_0_lang, index_vue_vue_type_style_index_0_lang];
-
-export { indexStyles_e1dfe05b as default };
-//# sourceMappingURL=index-styles.e1dfe05b.mjs.map
