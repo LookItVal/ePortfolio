62bdf668e7ea6e8b9ac7eae0c1404c3a628a1301
LookItVal <86853474+LookItVal@users.noreply.github.com>
2023-12-10 13:33:21 -0600

    portfolio for DS210

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,20 @@
 <template>
     <div class="home-window">
-        <h1>Quinn Valencia Cecil</h1>
+        <div class="header">
+            <h1>Quinn Valencia Cecil</h1>
+        </div>
+        <div class="temp-portfolio">
+            <h2>DS210: Intro to Data Science</h2>
+            <p>
+                To view the project, click <a href="https://docs.google.com/document/d/1wv9LefGa38sqqZ3cvvaTMCDypUtLAcbx/export?format=pdf">here</a>
+            </p>
+        </div>
+        <div class="bottom-note">
+            <p>
+                Note: this website is under construction and is not even the branch that I have worked on the most.
+                The website will not look like this by the end of development and I plan to make it far more elaborate
+            </p>
+        </div>
     </div>
 </template>
 
@@ -8,8 +22,17 @@
     .home-window {
         display: flex;
         align-items: center;
-        justify-content: center;
+        flex-direction: column;
+        flex-wrap: wrap;
+        justify-content: space-around;
         height: 100vh;
         width: 100vw;
+    }
+    .temp-portfolio {
+        display: flex;
+        align-items: center;
+        flex-direction: column;
+        flex-wrap: wrap;
+        justify-content: space-around;
     }
 </style>
