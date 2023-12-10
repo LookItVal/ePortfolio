3729f129de07814ea2ff7752e62e6f926acc0b9b
LookItVal <86853474+LookItVal@users.noreply.github.com>
2023-12-10 13:07:33 -0600

    base nuxt

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="home-window">
+        <h1>Quinn Valencia Cecil</h1>
+    </div>
+</template>
+
+<style>
+    .home-window {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        height: 100vh;
+        width: 100vw;
+    }
+</style>
